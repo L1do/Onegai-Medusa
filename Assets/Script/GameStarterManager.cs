f45573cf1efaf9e066568aa26a6632e89ac453c8
DESKTOP-OC75RC4\lawre <lawrencemoshani@gmail.com>
2018-11-06 21:33:32 +0900

    no message

--- Assets/Script/GameStarterManager.cs
+++ Assets/Script/GameStarterManager.cs
@@ -11,6 +11,7 @@ public class GameStarterManager : MonoBehaviour {
 	GameObject firstGear;
 
 	GameObject zeroGear;
+	float countTimer = 0;
 
 
 	// Use this for initialization
@@ -21,26 +22,47 @@ public class GameStarterManager : MonoBehaviour {
 		RightHand = GameObject.FindGameObjectWithTag("RightHand");
 		LeftHand = GameObject.FindGameObjectWithTag("LeftHand");
 	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Start Game");
+	}
 	void Start () {
 
-			//firstGear.gameObject.SetActive(true);
-			//zeroGear.gameObject.SetActive(false);	
-		
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	void OnTriggerEnter(Collider other)
-    {
-		firstGear.gameObject.SetActive(true);
-		zeroGear.gameObject.SetActive(false);	
-		Debug.Log ("Start Game");
+	//void OnTriggerEnter(Collider other)
+   // {
+	//	Debug.Log ("Start Game");
+	//	firstGear.gameObject.SetActive(true);
+	//	zeroGear.gameObject.SetActive(false);
+		//countTimer = Time.time + 5;
+
+		/*if (countTimer == 0)
+            return;
+			zeroGear.gameObject.SetActive(true);
+			firstGear.gameObject.SetActive(false);
+
+        if (Time.time > countTimer)
+        {
+            countTimer = 0;
+           
+        }
+        else
+        {
+            Debug.Log("Counting down " + (countTimer - Time.time));
+        }*/
+
+		//Debug.Log ("Start Game");
 //		SpeedManager.gameObject.SetActive(true);	
 			
 		           
-    }
+  //  }
 
 
 }
