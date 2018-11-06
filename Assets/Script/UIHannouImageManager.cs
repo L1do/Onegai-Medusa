7e6b596846e0a1def83fc23fab1a6b2444a7817a
DESKTOP-OC75RC4\lawre <lawrencemoshani@gmail.com>
2018-11-07 08:27:09 +0900

    no message

--- Assets/Script/UIHannouImageManager.cs
+++ Assets/Script/UIHannouImageManager.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class UIHannouImageManager : MonoBehaviour {
 	// get all UImages That I need
 	public Image GoodHannouImage;
 	public Image BadHannouImage;
 	// get the script the handles the "Ashiba" Shader
+//public AudioSource audioSource;
+	public AudioSource GoodHannouSFX;
+	public AudioSource BadHannouSFX;
 
 	public bool IsCollided = false;
 	public float fadeSpeed = 10f;
 	//public AshibaShaderManager AshibaShaderManager;
-
+    void Awake()
+	{
+		GoodHannouSFX = GetComponent<AudioSource>();
+		BadHannouSFX = GetComponent<AudioSource>();
+	}
 
 	
 	void Start()
@@ -28,17 +36,23 @@ public class UIHannouImageManager : MonoBehaviour {
 		//BadHannouImage.transform.Translate(Vector2.up* fadeSpeed * Time.deltaTime);
 
 		GoodHannouImage.gameObject.SetActive(true);
+		GoodHannouSFX.Play();
+		
 		IsCollided = true;
 
 		if(IsCollided = false)
 		{
 			BadHannouImage.gameObject.SetActive(true);
+			BadHannouSFX.Play();
 		}
 	}
 
 	void OnTriggerExit(Collider other)
 	{
 		GoodHannouImage.gameObject.SetActive(false);
+		BadHannouImage.gameObject.SetActive(false);
+
+		
 
 	}
 
