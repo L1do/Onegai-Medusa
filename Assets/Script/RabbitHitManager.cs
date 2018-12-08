ff46b83afbb8ab0a007f281e545ba1126333f7c1
lwando moshani <lawrencemoshani@gmail.com>
2018-12-08 15:33:15 +0900

    no message

--- Assets/Script/RabbitHitManager.cs
+++ Assets/Script/RabbitHitManager.cs
@@ -29,20 +29,35 @@ public class RabbitHitManager : MonoBehaviour {
 	{
 		Debug.Log ("RabbitHitManager.hitCheck");
 
-		if (Foot_L.IsHit == true && Foot_R.IsHit == false) {
-			Debug.Log ("左足ぶつかった");
+		if (Foot_L.IsHit == true && Foot_R.IsHit == false/* && Hand_L.IsHit == true && Hand_R.IsHit == true*/) {
+			Debug.Log ("左足クリア");
 			GetComponent<Animator>().SetTrigger("LeftJump");
 		}
-
-		if (Foot_L.IsHit == false && Foot_R.IsHit == true) {
-			Debug.Log ("右足ぶつかった");
+			
+		if (Foot_L.IsHit == false && Foot_R.IsHit == true/* && Hand_L.IsHit == true && Hand_R.IsHit == true*/) {
+			Debug.Log ("右足クリア");
 			GetComponent<Animator>().SetTrigger("RightJump");
 		}
 
-		if (Foot_L.IsHit == true && Foot_R.IsHit == true)
+		if (Foot_L.IsHit == true && Foot_R.IsHit == true/* && Hand_L.IsHit == true && Hand_R.IsHit == true*/)
 		{
-			Debug.Log ("両足ぶつかった");
+			Debug.Log ("両足クリア");
 			GetComponent<Animator>().SetTrigger("Jump");
 		}
+
+		/*if (Foot_L.IsHit == true && Foot_R.IsHit == false && Hand_L.IsHit == false && Hand_R.IsHit == false) {
+			Debug.Log ("左足ミス");
+			GetComponent<Animator>().SetTrigger("");
+		}
+
+		if (Foot_L.IsHit == true && Foot_R.IsHit == false && Hand_L.IsHit == false && Hand_R.IsHit == false) {
+			Debug.Log ("左足ミス");
+			GetComponent<Animator>().SetTrigger("");
+		}
+
+		if (Foot_L.IsHit == true && Foot_R.IsHit == true && Hand_L.IsHit == false && Hand_R.IsHit == false) {
+			Debug.Log ("両足ミス");
+			GetComponent<Animator>().SetTrigger("");
+		}*/
 	}
 }
